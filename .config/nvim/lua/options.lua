f25c1da0320715e0af2236a3c9ee1b963b86c5ca
André Anundsson <andre@anundsson.net>
2025-06-08 20:33:05 +0200

    added nvim config (#2)
    * 0.1 nvim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+vim.g.maplocalleader = ","
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = false
+vim.opt.smartcase = false
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.relativenumber = true
+vim.opt.undofile = true
